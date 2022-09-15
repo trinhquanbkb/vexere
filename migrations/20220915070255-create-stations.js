6954527e5e221f7f1fb3769abfbd14b3f834ad2a
trinh quan <quan20028a3kb@gmail.com>
2022-09-15 15:10:28 +0700

    master: add file

---
+++ migrations/20220915070255-create-stations.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Stations', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      address: {
+        type: Sequelize.STRING
+      },
+      province: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Stations');
+  }
+};
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const { sequelize } = require('./models/index')
+
+app.use(express.json())      // chuyển response và request về dạng json hết
+
+const publicPathDirectory = path.join(__dirname, './public')    //hàm join dùng để nối đường dẫn __dirname ở đây chính là đường dẫn tới file server.js và các file trong folder public
+app.use(express.static(publicPathDirectory))       //sử dụng đường dẫn đấy
+
+app.listen(3000, async () => {
+    // console.log(`listening http://localhost:3000`)
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+})
